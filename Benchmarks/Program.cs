6e5618648c188342f0f7c1a1a357d60cfc1c2ae8
Drawaes <seawardtim@gmail.com>
2017-03-12 22:51:23 +0000

    Added the ability to add more tests with "Test" suffix on the class
    Added nuget.config file with path to benchmarkdotnet's overnight repo in case it's needed

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,19 +1,48 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Benchmarks
 {
     class Program
     {
+        const string BenchmarkSuffix = "Tests";
+        
         static void Main(string[] args)
         {
-            //BenchmarkRunner.Run<TrimTests>();
-            //BenchmarkRunner.Run<TimeSpanTests>();
-            //BenchmarkRunner.Run<JSONTests>();
-            BenchmarkRunner.Run<ConditionalTests>();
+            var benchmarks = Assembly.GetCallingAssembly()
+                .DefinedTypes.Where(t => t.Name.EndsWith(BenchmarkSuffix))
+                .ToDictionary(t => t.Name.Substring(0, t.Name.Length - BenchmarkSuffix.Length), t => t, StringComparer.OrdinalIgnoreCase);
+
+            if (args.Length > 0 && args[0].Equals("all", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Running full benchmarks suite");
+                benchmarks.Select(pair => pair.Value).ToList().ForEach(action => BenchmarkRunner.Run(action));
+                return;
+            }
+
+            if (args.Length == 0 || !benchmarks.ContainsKey(args[0]))
+            {
+                Console.WriteLine("Please, select benchmark, list of available:");
+                benchmarks
+                    .Select(pair => pair.Key)
+                    .ToList()
+                    .ForEach(Console.WriteLine);
+                Console.WriteLine("All");
+                return;
+            }
+
+            BenchmarkRunner.Run(benchmarks[args[0]]);
+
+            Console.Read();
         }
     }
+
+
 
     class Config : ManualConfig
     {
