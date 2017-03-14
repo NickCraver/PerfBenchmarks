872cd8a8f1142d1e3e09a3626242c5c0af927d0d
Drawaes <seawardtim@gmail.com>
2017-03-14 22:15:36 +0000

    Static constructor test

---
+++ Benchmarks/StaticConstructorTests.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    /// <summary>
+    /// This test shows that the JIT can eliminate (turn into consts) static readonly
+    /// But it cannot if you put in an explicit static constructor instead it has to make
+    /// a check on each call that the constructor has already run
+    /// </summary>
+    [Config(typeof(Config))]
+    public class StaticConstructorsTests
+    {
+        const int Operations = 10000000;
+
+        [Benchmark(OperationsPerInvoke = Operations)]
+        public int WithStaticConstructor()
+        {
+            int returnValue = 0;
+            for(int i = 0; i < Operations;i++)
+            {
+                returnValue += Constructor.Number;
+            }
+            return returnValue;
+        }
+
+        [Benchmark(Baseline =true, OperationsPerInvoke = Operations)]
+        public int WithoutStaticConstructor()
+        {
+            int returnValue = 0;
+            for (int i = 0; i < Operations; i++)
+            {
+                returnValue += NoConstructor.Number;
+            }
+            return returnValue;
+        }
+
+        public static class Constructor
+        {
+            static Constructor()
+            {
+                Number = Environment.ProcessorCount;
+            }
+
+            public static readonly int Number;
+        }
+
+        public static class NoConstructor
+        {
+            public static readonly int Number = Environment.ProcessorCount;
+        }
+
+    }
+}
