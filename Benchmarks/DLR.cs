8abddb11e47c52c13f6d22d97acba17288a746d4
Nick Craver <craver@stackoverflow.com>
2017-03-21 08:37:05 -0400

    Add DLR (dynamic) vs. casting test

---
+++ Benchmarks/DLR.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class DLRTests
+    {
+        private static SomeType _type = new SomeType
+        {
+            Foo = nameof(SomeType.Foo),
+            Foo2 = nameof(SomeType.Foo2),
+            Conn = new DbConn
+            {
+                Name = "Test",
+                ConnectionString = "MyServer"
+            }
+        };
+        private static object _passed = _type;
+        
+        [Benchmark]
+        public string Casting()
+        {
+            var t = ((SomeType)_passed);
+            return t.Conn.ConnectionString;
+        }
+        
+        [Benchmark]
+        public string Dynamic()
+        {
+            dynamic t = _passed;
+            return t.Conn.ConnectionString;
+        }
+
+
+        public class SomeType
+        {
+            public string Foo { get; set; }
+            public string Foo2 { get; set; }
+            public DbConn Conn { get; set; }
+        }
+
+        public class DbConn
+        {
+            public string Name { get; set; }
+            public string ConnectionString { get; set; }
+        }
+    }
+}
--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -48,7 +48,7 @@ namespace Benchmarks
     {
         public Config()
         {
-            //Add(new MemoryDiagnoser());
+            Add(new MemoryDiagnoser());
             //Add(new InliningDiagnoser());
         }
     }
