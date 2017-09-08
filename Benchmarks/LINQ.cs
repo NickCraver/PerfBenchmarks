23a70f70331663d93aa9f8c18ad88d022fd757bb
Nick Craver <craver@stackoverflow.com>
2017-09-08 13:56:55 -0400

    Add .Count vs .Count()

---
+++ Benchmarks/LINQ.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class LINQTests
+    {
+        private readonly List<string> _items = Enumerable.Range(0, 50000).Select(i => "Num" + i).ToList();
+
+        [Benchmark(Description = ".Count()")]
+        public void Count()
+        {
+            var i = _items.Count();
+        }
+
+        [Benchmark(Description = ".Count")]
+        public void RawCount()
+        {
+            var i = _items.Count;
+        }
+    }
+}
