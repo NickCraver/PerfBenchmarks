afc051732b01826d17ba2b86109890152437f9a8
Nick Craver <nrcraver@gmail.com>
2018-02-01 20:27:49 -0500

    Add exception benchmarks
    Ben's gonna kill me

---
+++ Benchmarks/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class ExceptionTests
+    {
+        [Benchmark(Baseline = true)]
+        public string Baseline()
+        {
+            var ex = new Exception();
+            return ex.ToString();
+        }
+
+        [Benchmark]
+        public string Demystify()
+        {
+            var ex = new Exception().Demystify();
+            return ex.ToString();
+        }
+    }
+}
