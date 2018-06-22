dd576130e0b1f7d479f8c5d390898d234141f5b2
Nick Craver <nrcraver@gmail.com>
2018-06-22 19:46:56 -0400

    Add Lambdas

---
+++ Benchmarks/Lamdas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class LambdaTests
+    {
+        [Benchmark(Baseline = true)]
+        public void Direct() => Console.Write("");
+
+        [Benchmark]
+        public void ViaLambda() => Step(() => Console.Write(""));
+
+        private static void Step(Action action) => action();
+    }
+}
