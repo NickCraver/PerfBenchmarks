acf30dac71131b0e9cc80b0558dea3810921b8ed
Nick Craver <craver@stackoverflow.com>
2018-12-04 08:56:42 -0500

    Quick allocations benchmark

---
+++ Benchmarks/Allocations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class AllocationTests
+    {
+        [Benchmark(Description = "HashSet<string>")]
+        public HashSet<string> HashSetString() => new HashSet<string>();
+
+        [Benchmark]
+        public StringBuilder StringBuilder() => new StringBuilder();
+    }
+}
