9675ea451b9030a5e01494e6e379700732bf4975
Nick Craver <nrcraver@gmail.com>
2019-02-22 06:10:51 -0500

    Language detection benchmarks

---
+++ Benchmarks/LanguageDetection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using LanguageDetection;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class LanguageDetectionTests
+    {
+        private static LanguageDetector EnglishDetector;
+        private static LanguageDetector AllDetector;
+
+        public LanguageDetectionTests()
+        {
+            var ed = new LanguageDetector();
+            ed.AddLanguages("eng");
+            EnglishDetector = ed;
+
+            var ad = new LanguageDetector();
+            ad.AddLanguages("spa", "fra", "deu", "jpn", "por", "ukr", "zho", "ita", "rus", "kor");
+            AllDetector = ad;
+        }
+
+        //[Benchmark]
+        public LanguageDetector EnglishLoad()
+        {
+            var d = new LanguageDetector();
+            d.AddLanguages("eng");
+            return d;
+        }
+
+        //[Benchmark]
+        public LanguageDetector AllLoad()
+        {
+            var d = new LanguageDetector();
+            d.AddLanguages("spa", "fra", "deu", "jpn", "por", "ukr", "zho", "ita", "rus", "kor");
+            return d;
+        }
+
+        [Benchmark]
+        public string EnglishOnlyDetect() => EnglishDetector.Detect("Schneller brauner Fuchs springt über den faulen Hund");
+
+        [Benchmark]
+        public string AllDetect() => AllDetector.Detect("Schneller brauner Fuchs springt über den faulen Hund");
+    }
+}
