f16964a7c45c56096f574d3805417baa831d1afb
Nick Craver <craver@stackoverflow.com>
2020-03-08 21:25:22 -0400

    IPNet: More benchmarks, profiling, and prototyping a faster version
    Needs a lot more tests for completeness but the basics are in.
    
    Original benchmarks and Net472 are commented out for speed but easy to pull back in as a comparison.

---
+++ Profiling/Program.cs
@@ -0,0 +1,28 @@
+﻿using Benchmarks.Libs;
+using System.Diagnostics;
+using static System.Console;
+
+namespace Benchmarks
+{
+    public static class Program
+    {
+        const int loops = 50_000_000;
+
+        public static void Main(string[] args)
+        {
+            WriteLine("Press any key to begin");
+            ReadKey();
+
+            WriteLine("Running Loop ({0} iteations)...", loops);
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < loops; i++)
+            {
+                IPNet.Parse("127.0.0.1/16");
+            }
+            sw.Stop();
+            WriteLine("Done: " + sw.ElapsedMilliseconds + " ms");
+            //WriteLine("Press any key to exit");
+            //ReadKey();
+        }
+    }
+}
