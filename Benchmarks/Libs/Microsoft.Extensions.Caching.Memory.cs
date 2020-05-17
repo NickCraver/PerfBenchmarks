6ffce0921653a9686263b61540027c25a033b651
Nick Craver <craver@stackoverflow.com>
2020-05-17 10:15:17 -0400

    MemoryCache: initial experiments

---
+++ Benchmarks/Libs/Microsoft.Extensions.Caching.Memory.cs
@@ -0,0 +1,266 @@
+﻿// From https://raw.githubusercontent.com/dotnet/runtime/master/src/libraries/Microsoft.Extensions.Caching.Memory/src/
+//
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace Benchmarks.Libs
+{
+    /// <summary>
+    /// An implementation of <see cref="IMemoryCache"/> using a dictionary to
+    /// store its entries.
+    /// </summary>
+    public class MemoryCache
+    {
+        private readonly ConcurrentDictionary<string, CacheEntry> _entries;
+        private bool _disposed;
+
+        private readonly MemoryCacheOptions _options;
+        private DateTime _lastExpirationScan;
+
+        /// <summary>
+        /// Creates a new <see cref="MemoryCache"/> instance.
+        /// </summary>
+        /// <param name="optionsAccessor">The options of the cache.</param>
+        public MemoryCache(IOptions<MemoryCacheOptions> optionsAccessor)
+        {
+            _ = optionsAccessor ?? throw new ArgumentNullException(nameof(optionsAccessor));
+
+            _options = optionsAccessor.Value;
+            _entries = new ConcurrentDictionary<string, CacheEntry>();
+            _lastExpirationScan = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Cleans up the background collection events.
+        /// </summary>
+        ~MemoryCache() => Dispose(false);
+
+        /// <summary>
+        /// Gets the count of the current entries for diagnostic purposes.
+        /// </summary>
+        public int Count => _entries.Count;
+
+        private ICollection<KeyValuePair<string, CacheEntry>> EntriesCollection => _entries;
+
+        private void SetEntry(string key, CacheEntry entry)
+        {
+            if (_entries.TryGetValue(key, out CacheEntry priorEntry))
+            {
+                priorEntry.SetExpired(EvictionReason.Replaced);
+            }
+
+            if (!entry.CheckExpired())
+            {
+                bool entryAdded;
+                if (priorEntry == null)
+                {
+                    // Try to add the new entry if no previous entries exist.
+                    entryAdded = _entries.TryAdd(key, entry);
+                }
+                else
+                {
+                    // Try to update with the new entry if a previous entries exist.
+                    entryAdded = _entries.TryUpdate(key, entry, priorEntry);
+
+                    if (!entryAdded)
+                    {
+                        // The update will fail if the previous entry was removed after retrival.
+                        // Adding the new entry will succeed only if no entry has been added since.
+                        // This guarantees removing an old entry does not prevent adding a new entry.
+                        entryAdded = _entries.TryAdd(key, entry);
+                    }
+                }
+
+                if (!entryAdded)
+                {
+                    entry.SetExpired(EvictionReason.Replaced);
+                }
+            }
+            else
+            {
+                if (priorEntry != null)
+                {
+                    RemoveEntry(key, priorEntry);
+                }
+            }
+
+            // TODO: Threadpool background cache purging
+            //StartScanForExpiredItems();
+        }
+
+        /// <inheritdoc />
+        public bool TryGetValue(string key, out object result)
+        {
+            _ = key ?? throw new ArgumentNullException(nameof(key));
+
+            if (_entries.TryGetValue(key, out CacheEntry entry))
+            {
+                // Check if expired due to expiration tokens, timers, etc. and if so, remove it.
+                // Allow a stale Replaced value to be returned due to concurrent calls to SetExpired during SetEntry.
+
+                if (entry.CheckExpired() && entry.EvictionReason != EvictionReason.Replaced)
+                {
+                    // TODO: For efficiency queue this up for batch removal
+                    RemoveEntry(key, entry);
+                }
+                else
+                {
+                    entry.AccessCount++;
+                    result = entry.Value;
+                    return true;
+                }
+            }
+            result = null;
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void Remove(string key)
+        {
+            _ = key ?? throw new ArgumentNullException(nameof(key));
+
+            if (_entries.TryRemove(key, out CacheEntry entry))
+            {
+                entry.SetExpired(EvictionReason.Removed);
+            }
+
+            StartScanForExpiredItems();
+        }
+
+        private void RemoveEntry(string key, CacheEntry entry) => 
+            EntriesCollection.Remove(new KeyValuePair<string, CacheEntry>(key, entry));
+
+        // Called by multiple actions to see how long it's been since we last checked for expired items.
+        // If sufficient time has elapsed then a scan is initiated on a background task.
+        private void StartScanForExpiredItems()
+        {
+            var now = DateTime.UtcNow;
+            if (_options.ExpirationScanFrequency < now - _lastExpirationScan)
+            {
+                _lastExpirationScan = now;
+                Task.Factory.StartNew(state => ScanForExpiredItems((MemoryCache)state), this,
+                    CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+            }
+        }
+
+        private static void ScanForExpiredItems(MemoryCache cache)
+        {
+            var now = DateTime.UtcNow;
+            foreach (var pair in cache._entries)
+            {
+                if (pair.Value.CheckExpired(now))
+                {
+                    cache.RemoveEntry(pair.Key, pair.Value);
+                }
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    GC.SuppressFinalize(this);
+                }
+
+                _disposed = true;
+            }
+        }
+
+        // From Extensions
+        public TItem Set<TItem>(string key, TItem value, DateTime absoluteExpiration)
+        {
+            _ = key ?? throw new ArgumentNullException(nameof(key));
+            var entry = new CacheEntry
+            {
+                AbsoluteExpiration = absoluteExpiration,
+                Value = value,
+            };
+            SetEntry(key, entry);
+
+            return value;
+        }
+
+        public object Get(string key) => TryGetValue(key, out object value) ? value : null;
+        public TItem Get<TItem>(string key) => TryGetValue(key, out object value) ? (TItem)value : default;
+    }
+
+    public class CacheEntry
+    {
+        public object Value { get; internal set; }
+        public DateTime AbsoluteExpiration { get; internal set; }
+        internal int AccessCount;
+        internal EvictionReason EvictionReason { get; private set; }
+        private bool _isExpired;
+
+        internal CacheEntry() { }
+
+        internal bool CheckExpired() => _isExpired || CheckForExpiredTime(DateTime.UtcNow);
+        internal bool CheckExpired(DateTime now) => _isExpired || CheckForExpiredTime(now);
+
+        internal void SetExpired(EvictionReason reason)
+        {
+            if (EvictionReason == EvictionReason.None)
+            {
+                EvictionReason = reason;
+            }
+            _isExpired = true;
+        }
+
+        private bool CheckForExpiredTime(DateTime now)
+        {
+            if (AbsoluteExpiration <= now)
+            {
+                SetExpired(EvictionReason.Expired);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class MemoryCacheOptions : IOptions<MemoryCacheOptions>
+    {
+        /// <summary>
+        /// Gets or sets the minimum length of time between successive scans for expired items.
+        /// </summary>
+        public TimeSpan ExpirationScanFrequency { get; set; } = TimeSpan.FromMinutes(2);
+
+        MemoryCacheOptions IOptions<MemoryCacheOptions>.Value => this;
+    }
+
+    public enum EvictionReason : byte
+    {
+        None,
+
+        /// <summary>
+        /// Manually
+        /// </summary>
+        Removed,
+
+        /// <summary>
+        /// Overwritten
+        /// </summary>
+        Replaced,
+
+        /// <summary>
+        /// Timed out
+        /// </summary>
+        Expired,/*
+
+        /// <summary>
+        /// Overflow
+        /// </summary>
+        Capacity,*/
+    }
+}
--- Benchmarks/MemoryCache.cs
+++ Benchmarks/MemoryCache.cs
@@ -1,18 +1,32 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Configs;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using System;
 
+using SlimCache = Benchmarks.Libs.MemoryCache;
 using SysCache = System.Runtime.Caching.MemoryCache;
 using ExtCache = Microsoft.Extensions.Caching.Memory.MemoryCache;
+using System.Collections.Concurrent;
 
 namespace Benchmarks
 {
     [Config(typeof(Config))]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     public class MemoryCacheTests
     {
         // 3 sets of cache sizes
+        public static ConcurrentDictionary<string, object> 
+                                DictEmpty = new ConcurrentDictionary<string, object>(),
+                                DictSm = new ConcurrentDictionary<string, object>(),
+                                DictMd = new ConcurrentDictionary<string, object>(),
+                                DictLg = new ConcurrentDictionary<string, object>();
+
+        public static SlimCache SlimCacheEmpty = new SlimCache(new Libs.MemoryCacheOptions()),
+                                SlimCacheSm = new SlimCache(new Libs.MemoryCacheOptions()),
+                                SlimCacheMd = new SlimCache(new Libs.MemoryCacheOptions()),
+                                SlimCacheLg = new SlimCache(new Libs.MemoryCacheOptions());
+
         public static SysCache SysCacheEmpty = new SysCache("Empty"),
                                SysCacheSm = new SysCache("Small"),
                                SysCacheMd = new SysCache("Medium"),
@@ -23,51 +37,99 @@ namespace Benchmarks
                                ExtCacheMd = new ExtCache(Options.Create(new MemoryCacheOptions())),
                                ExtCacheLg = new ExtCache(Options.Create(new MemoryCacheOptions()));
         public static DateTimeOffset Expires { get; } = DateTime.UtcNow.AddDays(7);
+        public static DateTime ExpiresDt { get; } = DateTime.UtcNow.AddDays(7);
 
         static MemoryCacheTests()
         {
             for (int i = 0; i < 1000; i++)
             {
-                SysCacheSm.Add(i.ToString(), i.ToString(), Expires);
-                ExtCacheSm.Set(i.ToString(), i.ToString(), Expires);
+                var val = i.ToString();
+                DictSm[val] = val;
+                SlimCacheSm.Set(val, val, ExpiresDt);
+                SysCacheSm.Add(val, val, Expires);
+                ExtCacheSm.Set(val, val, Expires);
             }
 
             //for (int i = 0; i < 100_000; i++)
             //{
-            //    SysCacheMd.Add(i.ToString(), i.ToString(), Expires);
-            //    ExtCacheMd.Set(i.ToString(), i.ToString(), Expires);
+            //    var val = i.ToString();
+            //    DictMd[val] = val;
+            //    SlimCacheMd.Set(val, val, ExpiresDt);
+            //    SysCacheMd.Add(val, val, Expires);
+            //    ExtCacheMd.Set(val, val, Expires);
             //}
 
             //for (int i = 0; i < 1_000_000; i++)
             //{
-            //    SysCacheLg.Add(i.ToString(), i.ToString(), Expires);
-            //    ExtCacheLg.Set(i.ToString(), i.ToString(), Expires);
+            //    var val = i.ToString();
+            //    DictLg[val] = val;
+            //    SlimCacheLg.Set(val, val, ExpiresDt);
+            //    SysCacheLg.Add(val, val, Expires);
+            //    ExtCacheLg.Set(val, val, Expires);
             //}
         }
 
-        [Benchmark(Description = "SysGet (string) - Small")]
+        [Benchmark, BenchmarkCategory("Get (string) - Small")]
+        public string ConCurDictGetSm() => DictSm.TryGetValue("123", out var val) ? (string)val : null;
+
+        [Benchmark, BenchmarkCategory("Get (object) - Small")]
+        public object ConCurDictGetSmObject() => DictSm.TryGetValue("123", out var val) ? val : null;
+
+        //[Benchmark, BenchmarkCategory("Get (string) - Medium")]
+        //public string ConCurDictGetMd() => DictMd.TryGetValue("1234", out var val) ? (string)val : null;
+
+        //[Benchmark, BenchmarkCategory("Get (string) - Large")]
+        //public string ConCurDictGetLg() => DictLg.TryGetValue("12345", out var val) ? (string)val : null;
+
+        [Benchmark, BenchmarkCategory("Set (string) - Small")]
+        public void ConCurDictSetSm() => DictSm["1234"] = "1234";
+
+
+        [Benchmark, BenchmarkCategory("Get (string) - Small")]
+        public string SlimGetSm() => SlimCacheSm.Get<string>("123");
+
+        [Benchmark, BenchmarkCategory("Get (object) - Small")]
+        public object SlimGetSmObject() => SlimCacheSm.Get("123");
+
+        //[Benchmark, BenchmarkCategory("Get (string) - Medium")]
+        //public string SlimGetMd() => (string)SlimCacheMd.Get("1234");
+
+        //[Benchmark, BenchmarkCategory("Get (string) - Large")]
+        //public string SlimGetLg() => (string)SlimCacheLg.Get("12345");
+
+        [Benchmark, BenchmarkCategory("Set (string) - Small")]
+        public void SlimSetSm() => SlimCacheSm.Set("1234", "1234", ExpiresDt);
+
+
+        [Benchmark(Baseline = true), BenchmarkCategory("Get (string) - Small")]
         public string SysGetSm() => (string)SysCacheSm.Get("123");
 
-        [Benchmark(Description = "SysGet (object) - Small")]
+        [Benchmark(Baseline = true), BenchmarkCategory("Get (object) - Small")]
         public object SysGetSmObject() => SysCacheSm.Get("123");
 
-        //[Benchmark(Description = "SysGet - Medium")]
+        //[Benchmark(Baseline = true), BenchmarkCategory("Get (string) - Medium")]
         //public string SysGetMd() => (string)SysCacheMd.Get("1234");
 
-        //[Benchmark(Description = "SysGet - Large")]
+        //[Benchmark(Baseline = true), BenchmarkCategory("Get (string) - Large")]
         //public string SysGetLg() => (string)SysCacheLg.Get("12345");
 
+        [Benchmark(Baseline = true), BenchmarkCategory("Set (string) - Small")]
+        public void SysSetSm() => SysCacheSm.Set("1234", "1234", Expires);
 
-        [Benchmark(Description = "ExtGet (string) - Small")]
+
+        [Benchmark, BenchmarkCategory("Get (string) - Small")]
         public string ExtGetSm() => ExtCacheSm.Get<string>("123");
 
-        [Benchmark(Description = "ExtGet (object) - Small")]
+        [Benchmark, BenchmarkCategory("Get (object) - Small")]
         public object ExtGetSmObject() => ExtCacheSm.Get("123");
 
-        //[Benchmark(Description = "ExtGet - Medium")]
+        //[Benchmark, BenchmarkCategory("Get (string) - Medium")]
         //public string ExtGetMd() => ExtCacheMd.Get<string>("1234");
 
-        //[Benchmark(Description = "ExtGet - Large")]
+        //[Benchmark, BenchmarkCategory("Get (string) - Large")]
         //public string ExtGetLg() => ExtCacheLg.Get<string>("12345");
+
+        [Benchmark, BenchmarkCategory("Set (string) - Small")]
+        public void ExtSetSm() => ExtCacheSm.Set("1234", "1234", Expires);
     }
 }
