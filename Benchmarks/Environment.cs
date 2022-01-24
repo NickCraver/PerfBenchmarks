b9dca7832920defa0e33d3a626f66a8adf7b1d9d
Nick Craver <nrcraver@gmail.com>
2022-01-24 11:13:53 -0500

    Add Environment/Finally

---
+++ Benchmarks/Environment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class EnvironmentTests
+	{
+		private EnvironmentCache PropCache;
+		private IDictionary Cached;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			var currentVars = Environment.GetEnvironmentVariables();
+			Cached = currentVars;
+			PropCache = new EnvironmentCache(currentVars);
+		}
+
+		[Benchmark]
+		public string GetEnvironmentVariable() => Environment.GetEnvironmentVariable("TEMP");
+
+		[Benchmark]
+		public string Dictionary() => Cached["TEMP"] as string;
+
+		[Benchmark]
+		public string Prop() => PropCache.TempDirectory;
+
+		[Benchmark]
+		public string PropLazy() => PropCache.TempDirectoryLazy;
+
+		public class EnvironmentCache
+		{
+			private readonly IDictionary Cached;
+			public EnvironmentCache(IDictionary variables)
+			{
+				Cached = variables;
+				TempDirectory = Get("TEMP");
+			}
+
+			public string TempDirectory { get; }
+
+			private string _tempDirectory;
+			public string TempDirectoryLazy => _tempDirectory ??= Get("TEMP");
+
+			public string Get(string variableName) => Cached[variableName] as string;
+		}
+	}
+}
---
+++ Benchmarks/Finally.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class FinallyTests
+	{
+		[Benchmark]
+		public int NoFinally()
+		{
+			int a = 0;
+			try
+			{
+				a = 2;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			return a;
+		}
+
+		[Benchmark]
+		public int EmptyFinally()
+		{
+			int a = 0;
+			try
+			{
+				a = 2;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			finally
+			{
+				NotPresentMethod();
+			}
+			return a;
+		}
+
+		[Conditional("ADKHADAJSKDA")]
+		private void NotPresentMethod() { }
+	}
+}
